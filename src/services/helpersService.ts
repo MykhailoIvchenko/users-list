72fdb0409d1dee733037a1f8f42fbfa57b8268d9
Mykhailo <ivchenkomykhailo@gmail.com>
2025-08-05 08:50:10 +0300

    Implement the function for the api endpoint url obtaining

---
+++ src/services/helpersService.ts
@@ -0,0 +1,36 @@
+import { API_BASE_URL } from '../utils/consts';
+import { ApiEndpoints } from '../utils/enums';
+import { IQueryParam } from '../utils/types';
+
+function getApiEndpointUrl(
+  pathChunks: (string | number | ApiEndpoints)[],
+  queryParams?: IQueryParam[]
+): string {
+  const normalizedPath = pathChunks
+    .map((chunk) =>
+      String(chunk)
+        .trim()
+        .replace(/^\/+|\/+$/g, '')
+    )
+    .filter(Boolean)
+    .join('/');
+
+  const url = new URL(
+    `${API_BASE_URL}/${normalizedPath}`,
+    'https://dummyjson.com'
+  );
+
+  if (queryParams?.length) {
+    const params = new URLSearchParams();
+
+    for (const { name, value } of queryParams) {
+      params.append(name, String(value));
+    }
+
+    url.search = params.toString();
+  }
+
+  return url.pathname + url.search;
+}
+
+export const helpersService = { getApiEndpointUrl };
