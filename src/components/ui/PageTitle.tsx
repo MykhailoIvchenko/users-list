cab6caa661092f04e03b95a65264678fcee7ba3d
Mykhailo <ivchenkomykhailo@gmail.com>
2025-08-06 08:18:42 +0300

    Implement the component for the page title

---
+++ src/components/ui/PageTitle.tsx
@@ -0,0 +1,32 @@
+import React, { memo, ReactNode } from 'react';
+import styled from 'styled-components';
+import { media } from '../../styles/media';
+
+interface PageTitleProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const StyledPageTitle = styled.h1`
+  font-family: ${({ theme }) => theme.fonts.body};
+  font-size: ${({ theme }) => theme.fontSizes.xl};
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.text};
+  margin: ${({ theme }) => theme.spacing(4)} 0
+    ${({ theme }) => theme.spacing(3)};
+  line-height: 1.2;
+
+  ${media.mobile`
+    font-size: ${({ theme }) => theme.fontSizes.lg};
+    margin: ${({ theme }) => theme.spacing(3)} 0 ${({ theme }) =>
+    theme.spacing(2)};
+  `}
+
+  ${media.fullHD`
+    font-size: 2.5rem;
+  `}
+`;
+
+export const PageTitle = memo(({ children, className }: PageTitleProps) => (
+  <StyledPageTitle className={className}>{children}</StyledPageTitle>
+));
