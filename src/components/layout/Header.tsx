80398521e38ca5acfa7421fa9fe91b8b324a71b8
Mykhailo <ivchenkomykhailo@gmail.com>
2025-08-06 20:21:47 +0300

    Implement the header component

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,68 @@
+import { memo } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { media } from '../../styles/media';
+import { AppRoutes } from '../../utils/enums';
+import { BurgerButton } from '../BurgetButton';
+import { Navbar } from '../Navbar';
+import { NavRoute } from '../../utils/types';
+
+interface HeaderProps {
+  isMobileMenuOpen: boolean;
+  onToggleMobileMenu: () => void;
+  routes: NavRoute[];
+}
+
+const HeaderWrapper = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: ${({ theme }) => theme.spacing(2)} ${({ theme }) => theme.spacing(4)};
+  background-color: ${({ theme }) => theme.colors.surface};
+  position: relative;
+  z-index: 10;
+`;
+
+const Logo = styled(Link)`
+  font-size: ${({ theme }) => theme.fontSizes.lg};
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.primary};
+  text-decoration: none;
+`;
+
+const BurgerWrapper = styled.div`
+  display: flex;
+
+  ${media.tablet`
+    display: none;
+  `}
+`;
+
+const NavbarWrapper = styled.div`
+  display: none;
+
+  ${media.tablet`
+    display: flex;
+  `}
+`;
+
+export const Header = memo(
+  ({ isMobileMenuOpen, onToggleMobileMenu, routes }: HeaderProps) => {
+    return (
+      <HeaderWrapper>
+        <Logo to={AppRoutes.HOME}>MyApp</Logo>
+
+        <BurgerWrapper>
+          <BurgerButton
+            isOpen={isMobileMenuOpen}
+            onClick={onToggleMobileMenu}
+          />
+        </BurgerWrapper>
+
+        <NavbarWrapper>
+          <Navbar routes={routes} />
+        </NavbarWrapper>
+      </HeaderWrapper>
+    );
+  }
+);
